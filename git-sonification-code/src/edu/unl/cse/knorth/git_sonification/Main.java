b5ea2872867bad675eab246c85e3da3e241e5107
Kevin North <knorth@huskers.unl.edu>
2015-07-16 15:30:50 -0500

    Copied old GitSonifier project and made changes for new Netbeans config

--- git-sonification-code/src/edu/unl/cse/knorth/git_sonification/Main.java
+++ git-sonification-code/src/edu/unl/cse/knorth/git_sonification/Main.java
@@ -19,12 +19,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
 //        GregorianCalendar cal = new GregorianCalendar();
 //        cal.set(2009, 11, 23);
-        DateTime since = new DateTime(2009, 11, 4, 10, 0);
+        DateTime since = new DateTime(2009, 11, 4, 0, 0);
 //        cal.set(2010, 0, 6);
-        DateTime until = new DateTime(2009, 11, 5, 14, 0);
+        DateTime until = new DateTime(2009, 11, 9, 0, 0);
                 
         List<PartialCommit> partialCommits;
-        try(GitCaller gitCaller = new GitCaller("../../voldemort/.git")) {
+        try(GitCaller gitCaller = new GitCaller("../../../voldemort/.git")) {
             partialCommits = gitCaller.getPartialCommits();
         }
         
