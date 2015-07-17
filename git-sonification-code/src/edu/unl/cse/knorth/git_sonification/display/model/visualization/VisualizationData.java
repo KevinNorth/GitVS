250b35e3bc4fb31f9d299971b0cbf348493ec19e
enahsthezombie <enahsnalob@gmail.com>
2015-07-17 15:20:20 -0500

    added number of start and end branches.

--- git-sonification-code/src/edu/unl/cse/knorth/git_sonification/display/model/visualization/VisualizationData.java
+++ git-sonification-code/src/edu/unl/cse/knorth/git_sonification/display/model/visualization/VisualizationData.java
@@ -6,12 +6,22 @@ import java.util.Collection;
 
 public class VisualizationData {
   private List<Layer> layers;
+  private int numStart;
+  private int numEnd;
 
-  public VisualizationData(Collection<Layer> layers) {
+  public VisualizationData(Collection<Layer> layers, int numStart, int numEnd) {
     this.layers = new ArrayList<Layer>(layers);
+    this.numStart = numStart;
+    this.numEnd = numEnd;
   }
 
   public List<Layer> getLayers() {
     return layers;
+  }
+  public int getNumStart() {
+    return numStart;
+  }
+  public int getNumEnd() {
+    return numEnd;
   }
 }
