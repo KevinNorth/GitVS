1e9b38b14762ac77314c58793fab082059d3f511
enahsthezombie <enahsnalob@gmail.com>
2015-07-20 11:33:18 -0500

    added keyboard playhead movement with the W and S keys. potential frame rate issue without delta time.

--- git-sonification-code/src/edu/unl/cse/knorth/git_sonification/display/view/VisMain.java
+++ git-sonification-code/src/edu/unl/cse/knorth/git_sonification/display/view/VisMain.java
@@ -19,11 +19,13 @@ public class VisMain extends PApplet {
     float mouseStartY;
 
     float aspect;
+    float fps;
     float s;
     float s4;
     
     float yPos;
     float playHead;
+    float playSpeed;
     float rotateAmt;
 
     int left;
@@ -46,12 +48,15 @@ public class VisMain extends PApplet {
     @Override
     public void setup() {
         size(1200, 600, P3D);
+        fps = 30;
+        frameRate(fps);
         fill(200);
         aspect = (float) width / (float) height;
-        s = aspect * 10;
+        s = 30;
         s4 = 4*s;
-        yPos = 0;
+        playSpeed = (s4*25)/(60*fps);
         playHead = 0;
+        yPos = 0;
         rotateAmt = 0;
         left = 0;
         right = width;
@@ -66,62 +71,72 @@ public class VisMain extends PApplet {
         directionalLight(126, 126, 126, 0, 0, -1);
         ambientLight(102, 102, 102);
 
-        translate(0, yPos, 0);
+        translate(0, yPos, 0); //sets everything to be relitive to yPos
+        
+        //this is the playhead for the music
         strokeWeight(10);
+        if(keyPressed){
+            if(key == 'w'){
+                playHead -= playSpeed;
+            }else if(key == 's'){
+                playHead += playSpeed;
+            }
+        }
         line(left, playHead, -1000, right, playHead, -1000);
 
         pushMatrix();
-        translate(width / 2, s4, 0);
+        translate(width / 2, s4, 0); //centers the vis and starts it a unit down
         rotateY((rotateAmt / (float) width) * (2 * PI));
 
-        int numColors = visDat.getLayers().size();
-        int z = 0;
-        for (Layer layer : visDat.getLayers()) {
-            z++;
-            int y = 0;
-            for (Row row : layer.getRows()) {
-                y++;
-                strokeWeight(5);
-                stroke(z*255/numColors,z*255/numColors,z*255/numColors);
-                for (Line line : row.getOutgoingLines()) {
-                    if (line.isVisible) {
-                        line(line.fromBranch* s4, y* s4, z* s4, line.toBranch* s4, y* s4, z* s4);
-                    }
-                }
-                noStroke();
-                if (row.isVisable()) {
-                    pushMatrix();
-                    fill(color(z*255/numColors,z*255/numColors,z*255/numColors));
-                    translate(row.getBranchLocation() * s4, y * s4, z * s4);
-                    sphere(s);
-                    popMatrix();
-                }
-            }
-        }
-
-//        for (int z = 0; z < 3; z++) {
-//            noStroke();
-//            for (int i = 0; i < 7; i++) {
-//                for (int w = 0; w < 7; w++) {
-//                    mx[w] = w * 4 * s;
-//                    my[i] = i * 4 * s;
+//        int numColors = visDat.getLayers().size();
+//        int z = 0;
+//        for (Layer layer : visDat.getLayers()) {
+//            z++;
+//            int y = 0;
+//            for (Row row : layer.getRows()) {
+//                y++;
+//                strokeWeight(5);
+//                stroke(z*255/numColors,z*255/numColors,z*255/numColors);
+//                for (Line line : row.getOutgoingLines()) {
+//                    if (line.isVisible) {
+//                        line(line.fromBranch* s4, y* s4, z* s4, line.toBranch* s4, y* s4, z* s4);
+//                    }
+//                }
+//                noStroke();
+//                if (row.isVisable()) {
 //                    pushMatrix();
-//                    fill(color(30 + z * 50, 30 + z * 50, 30 + z * 50));
-//                    translate(mx[w], my[i], z * 4 * s);
+//                    fill(color(z*255/numColors,z*255/numColors,z*255/numColors));
+//                    translate(row.getBranchLocation() * s4, y * s4, z * s4);
 //                    sphere(s);
 //                    popMatrix();
 //                }
 //            }
-//            strokeWeight(5);
-//            stroke(30 + z * 50, 30 + z * 50, 30 + z * 50);
-//            for (int i = 0; i < 6; i++) {
-//                for (int w = 0; w < 6; w++) {
-//                    line(mx[w], my[i], z * 4 * s, mx[w + 1], my[i + 1], z * 4 * s);
-//                }
-//            }
 //        }
+
+        for (int z = 0; z < 3; z++) {
+            noStroke();
+            for (int i = 0; i < 7; i++) {
+                for (int w = 0; w < 7; w++) {
+                    mx[w] = w * s4;
+                    my[i] = i * s4;
+                    pushMatrix();
+                    fill(color(30 + z * 50, 30 + z * 50, 30 + z * 50));
+                    translate(mx[w], my[i], z * s4);
+                    sphere(s);
+                    popMatrix();
+                }
+            }
+            strokeWeight(5);
+            stroke(30 + z * 50, 30 + z * 50, 30 + z * 50);
+            for (int i = 0; i < 6; i++) {
+                for (int w = 0; w < 6; w++) {
+                    line(mx[w], my[i], z * s4, mx[w + 1], my[i + 1], z * s4);
+                }
+            }
+        }
+        
         popMatrix();
-        //System.out.println("playHead = " + playHead);
+        System.out.println("playHead = " + ((playHead/s4)+0.25));
     }
 
     @Override
