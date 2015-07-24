b05c51f8c39dfa560eea7c9175c2c8d9d18ba718
Kevin North <knorth@huskers.unl.edu>
2015-07-24 13:17:46 -0500

    Added GitDataProcessor class to encapsulate all git processing logic

---
+++ git-sonification-code/src/edu/unl/cse/knorth/git_sonification/GitDataProcessor.java
@@ -0,0 +1,47 @@
+package edu.unl.cse.knorth.git_sonification;
+
+import edu.unl.cse.knorth.git_sonification.data_collection.commit_processor.CommitProcessor;
+import edu.unl.cse.knorth.git_sonification.data_collection.components.Component;
+import edu.unl.cse.knorth.git_sonification.data_collection.components.ComponentDataParser;
+import edu.unl.cse.knorth.git_sonification.data_collection.conflict_data.Conflict;
+import edu.unl.cse.knorth.git_sonification.data_collection.conflict_data.ConflictDataParser;
+import edu.unl.cse.knorth.git_sonification.data_collection.git_caller.GitCaller;
+import edu.unl.cse.knorth.git_sonification.data_collection.git_caller.PartialCommit;
+import edu.unl.cse.knorth.git_sonification.data_collection.intermediate_data.Commit;
+import edu.unl.cse.knorth.git_sonification.data_processing.sonification.SonificationProcessor;
+import edu.unl.cse.knorth.git_sonification.data_processing.visualization.VisualizationProcessor;
+import edu.unl.cse.knorth.git_sonification.display.model.ViewModel;
+import edu.unl.cse.knorth.git_sonification.display.model.sonification.SonificationData;
+import edu.unl.cse.knorth.git_sonification.display.model.visualization.VisualizationData;
+import java.io.IOException;
+import java.util.List;
+import org.joda.time.DateTime;
+
+public class GitDataProcessor {
+    public ViewModel processGitData(String targetGitRepoLocation,
+            DateTime startDate, DateTime endDate) throws IOException {
+                List<PartialCommit> partialCommits;
+        try(GitCaller gitCaller = new GitCaller(targetGitRepoLocation)) {
+            partialCommits = gitCaller.getPartialCommits();
+        }
+        
+        List<Conflict> conflicts = new ConflictDataParser()
+                .parseConflictData("data/conflict_data.dat");
+        
+        List<Component> components = new ComponentDataParser()
+                .parseComponents("data/components.txt");
+        
+        List<Commit> commits = new CommitProcessor()
+                .processCommits(partialCommits, conflicts, components,
+                        startDate, endDate);
+                
+        SonificationProcessor sonificationProcessor = new SonificationProcessor();
+        SonificationData sonificationData = sonificationProcessor.processSonificationData(commits);
+                
+        VisualizationData visualizationData =
+                new VisualizationProcessor().produceVisualizationData(commits,
+                        components, sonificationData);
+        
+        return new ViewModel(visualizationData, sonificationData);
+    }
+}
--- git-sonification-code/src/edu/unl/cse/knorth/git_sonification/Main.java
+++ git-sonification-code/src/edu/unl/cse/knorth/git_sonification/Main.java
@@ -1,21 +1,11 @@
 package edu.unl.cse.knorth.git_sonification;
 
-import edu.unl.cse.knorth.git_sonification.data_collection.commit_processor.CommitProcessor;
-import edu.unl.cse.knorth.git_sonification.data_collection.components.Component;
-import edu.unl.cse.knorth.git_sonification.data_collection.components.ComponentDataParser;
-import edu.unl.cse.knorth.git_sonification.data_collection.conflict_data.Conflict;
-import edu.unl.cse.knorth.git_sonification.data_collection.conflict_data.ConflictDataParser;
 import edu.unl.cse.knorth.git_sonification.data_collection.git_caller.GitCaller;
-import edu.unl.cse.knorth.git_sonification.data_collection.git_caller.PartialCommit;
-import edu.unl.cse.knorth.git_sonification.data_collection.intermediate_data.Commit;
-import edu.unl.cse.knorth.git_sonification.data_processing.sonification.SonificationProcessor;
 import edu.unl.cse.knorth.git_sonification.data_processing.visualization.VisualizationProcessor;
-import edu.unl.cse.knorth.git_sonification.display.model.sonification.Measure;
-import edu.unl.cse.knorth.git_sonification.display.model.sonification.SonificationData;
+import edu.unl.cse.knorth.git_sonification.display.model.ViewModel;
 import edu.unl.cse.knorth.git_sonification.display.model.visualization.Layer;
 import edu.unl.cse.knorth.git_sonification.display.model.visualization.VisualizationData;
 import java.io.IOException;
-import java.util.List;
 import org.joda.time.DateTime;
 
 public class Main {
@@ -26,38 +16,12 @@ public class Main {
 //        cal.set(2010, 0, 6);
         DateTime until = new DateTime(2009, 11, 9, 0, 0);
                 
-        List<PartialCommit> partialCommits;
-        try(GitCaller gitCaller = new GitCaller("../../voldemort/.git")) {
-            partialCommits = gitCaller.getPartialCommits();
-        }
+        ViewModel viewModel =
+                new GitDataProcessor().processGitData("../../voldemort/.git",
+                        since, until);
         
-        List<Conflict> conflicts = new ConflictDataParser()
-                .parseConflictData("data/conflict_data.dat");
-        
-        List<Component> components = new ComponentDataParser()
-                .parseComponents("data/components.txt");
-        
-        List<Commit> commits = new CommitProcessor()
-                .processCommits(partialCommits, conflicts, components, since,
-                        until);
-        
-        for(Commit commit : commits) {
-            System.out.println(commit);
-        }
-        
-        System.out.println("\n----------------\n");
-        
-        SonificationProcessor sonificationProcessor = new SonificationProcessor();
-        SonificationData sonificationData = sonificationProcessor.processSonificationData(commits);
-        
-        for(Measure measure : sonificationData.getMeasures()) {
-            System.out.println(measure);
-        }
-        
-        VisualizationData visualizationData =
-                new VisualizationProcessor().produceVisualizationData(commits,
-                        components, sonificationData);
-        
+        VisualizationData visualizationData = viewModel.getVisualizationData();
+                
         for(Layer layer : visualizationData.getLayers()) {
             System.out.println(layer);
         }
