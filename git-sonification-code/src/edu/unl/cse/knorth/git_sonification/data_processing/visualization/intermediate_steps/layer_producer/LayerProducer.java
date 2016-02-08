82836e846ee3357bb5d31312440fb8f3faee0636
Kevin North <knorth@huskers.unl.edu>
2016-02-08 17:39:30 -0600

    Fixed conflict sounds
    Conflict sounds weren't being played correctly, so I gutted the old
    algorithm and replaced it with a much more straightforward one that
    works.

--- git-sonification-code/src/edu/unl/cse/knorth/git_sonification/data_processing/visualization/intermediate_steps/layer_producer/LayerProducer.java
+++ git-sonification-code/src/edu/unl/cse/knorth/git_sonification/data_processing/visualization/intermediate_steps/layer_producer/LayerProducer.java
@@ -1,9 +1,9 @@
 package edu.unl.cse.knorth.git_sonification.data_processing.visualization.intermediate_steps.layer_producer;
 
 import edu.unl.cse.knorth.git_sonification.data_collection.components.Component;
+import edu.unl.cse.knorth.git_sonification.data_collection.conflict_data.Conflict;
 import edu.unl.cse.knorth.git_sonification.data_processing.visualization.intermediate_steps.annotated_commits.AnnotatedCommit;
 import edu.unl.cse.knorth.git_sonification.data_processing.visualization.intermediate_steps.annotated_commits.AnnotatedCommitLine;
-import edu.unl.cse.knorth.git_sonification.display.model.sonification.Measure;
 import edu.unl.cse.knorth.git_sonification.display.model.sonification.SonificationData;
 import edu.unl.cse.knorth.git_sonification.display.model.visualization.Commit;
 import edu.unl.cse.knorth.git_sonification.display.model.visualization.Layer;
@@ -11,36 +11,42 @@ import edu.unl.cse.knorth.git_sonification.display.model.visualization.Line;
 import edu.unl.cse.knorth.git_sonification.display.model.visualization.Row;
 import edu.unl.cse.knorth.git_sonification.display.model.visualization.VisualizationData;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeComparator;
 
 public class LayerProducer {
     public VisualizationData produceLayers(List<AnnotatedCommit> commits,
-            SonificationData sonificationData, List<Component> components) {
+            SonificationData sonificationData, List<Component> components,
+            List<Conflict> conflicts) {
         final int numStartBranches = commits.get(0).getMaxFromBranchNumber();
         final int numEndBranches =
                 commits.get(commits.size() - 1).getMaxToBranchNumber();
+
+        List<Conflict> currentConflicts = new LinkedList<>();
         
         Layer combinedLayer = new Layer();
-        
-        Iterator<Measure> measureIter = sonificationData.getMeasures()
-                .iterator();
+
+        AnnotatedCommit previousCommit = null;
         for(AnnotatedCommit commit : commits) {
-            Measure currentMeasure = measureIter.next();
-
-            if(currentMeasure.isDaySeparator()) {
-                Measure lastDaySeparator;
-                do {
-                    lastDaySeparator = currentMeasure;
-                    currentMeasure = measureIter.next();
-                } while(currentMeasure.isDaySeparator());
-                
+            if((previousCommit != null) && (areTimestampsOnDifferentDays(
+                    previousCommit.getTimestamp(), commit.getTimestamp()))) {
                 addDaySeparatorRow(combinedLayer, commit,
-                        lastDaySeparator);
+                        currentConflicts.size());
             }
-
             
-            addRow(combinedLayer, commit, currentMeasure);
+            List<Conflict> newConflicts =
+                    findStartedConflicts(commit, conflicts);
+            currentConflicts.addAll(newConflicts);
+            
+            List<Conflict> resolvedConflicts = findResolvedConflicts(commit,
+                    conflicts);
+            currentConflicts.removeAll(resolvedConflicts);
+            
+            addRow(combinedLayer, commit, currentConflicts.size());
+            
+            previousCommit = commit;
         }
         
         return new VisualizationData(combinedLayer, components,
@@ -48,7 +54,7 @@ public class LayerProducer {
     }
         
     private void addRow(Layer combinedLayer, AnnotatedCommit commit,
-            Measure currentMeasure) {
+            int numConflicts) {
         List<Line> lines = new ArrayList<>(
                 commit.getIncomingBranches().size());
 
@@ -62,13 +68,12 @@ public class LayerProducer {
         }
                 
         Row row = new Row(commit.getAuthor(), commit.getTimestamp(),
-            commit.getBranch(), currentMeasure.getNumConflicts(),
-                modelCommit, lines);
+            commit.getBranch(), numConflicts, modelCommit, lines);
         combinedLayer.getRows().add(row);
     }
 
     private void addDaySeparatorRow(Layer combinedLayer, AnnotatedCommit commit,
-            Measure currentMeasure) {
+            int numConflicts) {
         List<Line> lines = new ArrayList<>(
             commit.getIncomingBranches().size());
         for(AnnotatedCommitLine oldLine : commit.getIncomingBranches()) {
@@ -76,8 +81,41 @@ public class LayerProducer {
                     oldLine.getFromBranch()));
         }
 
-        Row row = new Row(currentMeasure.getTimestamp(),
-            currentMeasure.getNumConflicts(), lines);
+        Row row = new Row(commit.getTimestamp().withTimeAtStartOfDay(),
+                numConflicts, lines);
         combinedLayer.getRows().add(row);
+    }
+
+    private boolean areTimestampsOnDifferentDays(DateTime timestamp1,
+            DateTime timestamp2) {
+        return DateTimeComparator.getDateOnlyInstance().compare(timestamp1,
+                timestamp2) != 0;
+    }
+
+    private List<Conflict> findStartedConflicts(AnnotatedCommit commit,
+            List<Conflict> conflicts) {
+        List<Conflict> startedConflicts = new LinkedList<>();
+        
+        for(Conflict conflict : conflicts) {
+            if(commit.getHash().equals(conflict.getCommitIntroducingConflict()))
+            {
+                startedConflicts.add(conflict);
+            }
+        }
+        
+        return startedConflicts;
+    }
+
+    private List<Conflict> findResolvedConflicts(AnnotatedCommit commit,
+            List<Conflict> conflicts) {
+        List<Conflict> resolvedConflicts = new LinkedList<>();
+        
+        for(Conflict conflict : conflicts) {
+            if(commit.getHash().equals(conflict.getCommitResolvingConflict())) {
+                resolvedConflicts.add(conflict);
+            }
+        }
+        
+        return resolvedConflicts;
     }
 }
