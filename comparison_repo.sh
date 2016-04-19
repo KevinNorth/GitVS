4e1ca1b8bbf369b1601ad48fe74b647b197b56ae
Kevin North <knorth@huskers.unl.edu>
2016-04-19 04:32:36 -0500

    Updated scripts for running GitVS on experiment machine

--- comparison_repo.sh
+++ comparison_repo.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd git-sonification-code
-java -jar dist/GitVS.jar 96372fa7a3c5a87eef9a97acada44ea17deb9306 21f56e91efd4af4a8b2c54d6ba6b50b717f2aca7 ../../storm/.git data/storm_conflicts.dat
+java -jar dist/GitVS.jar b930e100a9f74e832fd9142d0c825d9f24d0af1d d448e34812e5cbffe848ce88189a05df1be3226e ../../Storm/.git data/storm_conflicts.dat
